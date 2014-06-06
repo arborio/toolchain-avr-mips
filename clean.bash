d2e57d3a465e821be92bcb0426fc59a19b457c99
B. Griffin <bgriffin@arbor-io.com>
2014-06-05 20:28:13 -0700

    Added avr-libc

--- clean.bash
+++ clean.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
 
-rm -rf gcc-4.3.2 gmp-4.3.2 mpc-0.8.1 mpfr-2.4.2 binutils-2.20.1 *-build
+rm -rf gcc-4.3.2 gmp-4.3.2 mpc-0.8.1 mpfr-2.4.2 binutils-2.20.1 avr-libc-1.8.0 *-build
 
 rm -rf objdir/{info,man,share}
